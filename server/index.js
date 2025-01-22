f1ba967d65b17ed3ea2e598eca96b46b8fef1152
Anuj <anujshrivastav530@gmail.com>
2025-01-22 19:47:26 +0530

    Update CORS configuration to use production client URL from environment variables

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:5173", credentials:true }));
+app.use(cors({ origin: env.CLIENT_PRODUCTION || "http://localhost:5173", credentials:true }));
 dotenv.config();
 
 const PORT = 8000;
