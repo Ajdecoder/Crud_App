de31ac55361e210315f5e69771ca636f641e5063
Anuj <anujshrivastav530@gmail.com>
2025-01-22 20:30:39 +0530

    Refactor dotenv configuration placement in server index file

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import route from "./routes/user.Routes.js";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({ origin: env.CLIENT_PRODUCTION, credentials:true }));
-dotenv.config();
 
 const PORT = 8000;
 const URL = process.env.Atlas_MONGO_URL;
